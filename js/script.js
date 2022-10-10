8f2ce62dcf1161c5695e96077f12024ea66fbfd4
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-10-10 15:47:51 +0200

    Content on cells

--- js/script.js
+++ js/script.js
@@ -12,25 +12,27 @@ for(let i = 1; i <= 100; i++) {
     // IF %3 AND %5, note counter-both
     // ELSE IF %3, note counter-3
     // ELSE IF %5, note counter-5
+    const cell = document.createElement("div");
+    cell.classList.add("ms_cards");
     
     let fizzCounter = "";
     if ( i % 3 === 0 && i % 5 === 0) {
         fizzCounter = "counter-both";
+        cell.innerHTML = "FizzBuzz";
     }
     else if ( i % 3 === 0) {
         fizzCounter = "counter-3";
+        cell.innerHTML = "Fizz";
     }
     else if ( i % 5 === 0) {
-        fizzCounter = "counter-5"
+        fizzCounter = "counter-5";
+        cell.innerHTML = "Buzz";
+    } else {
+        cell.innerHTML = i;
     }
     
-    const cell = document.createElement("div");
-    cell.classList.add("ms_cards");
     cell.classList.add(`"${fizzCounter}"`);
     row.append(cell)
-    cell.innerHTML = i;
-    row.append(cell);
-
 
     console.log(cell);
 }
